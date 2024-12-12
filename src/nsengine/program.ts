2df86bf7217ef6fa0135960b51f1a89c952d51f8
Kameron brooks <kameron.cw.11@gmail.com>
2024-12-11 22:04:00 -0500

    Created basic project layout

---
+++ src/nsengine/program.ts
@@ -0,0 +1,8 @@
+export const OP_NOOP            = 0x00;
+export const OP_TERM            = 0x01;
+export const OP_JUMP            = 0x02;
+
+export const OP_BRANCH_TRUE     = 0x03;
+export const OP_BRANCH_FALSE    = 0x04;
+
+// ============= STACK OPERATIONS =============
