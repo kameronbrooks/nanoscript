3b76f6ae27582089215811b3f3a3f0de40ab4ed9
Kameron brooks <kameron.cw.11@gmail.com>
2024-12-28 09:21:06 -0500

    - updated package json

---
+++ src/npm-main.ts
@@ -0,0 +1,6 @@
+export {NSEngine} from "./nsengine/nsengine";
+export {Tokenizer} from "./nsengine/tokenizer";
+export {Interpreter} from "./nsengine/interpreter";
+export {Compiler} from "./nsengine/compiler";
+export {Nenv, NenvExport, NenvModule} from "./nsengine/nenv";
+export {builtin_module} from "./nenvmodules/builtin";
